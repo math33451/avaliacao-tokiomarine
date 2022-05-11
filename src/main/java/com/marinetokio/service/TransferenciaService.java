612b6fc88b9483137e2fd72f0a1ab779d2a0c725
Matheus Bezerra <murilomatheus20@gmail.com>
2022-05-11 11:50:13 -0300

    função salvar transferencia

--- src/main/java/com/marinetokio/service/TransferenciaService.java
+++ src/main/java/com/marinetokio/service/TransferenciaService.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import com.marinetokio.DTO.TransferenciaDTO;
 import com.marinetokio.dto.TransferenciaDto;
 import com.marinetokio.model.Transferencia;
 import com.marinetokio.repository.TransferenciaRepository;
@@ -81,6 +80,11 @@ public class TransferenciaService {
 		List<Transferencia> transferencia = transferenciaRepository.findAll();
 		return new ArrayList<>(transferencia.stream()
 				.map(transfer -> new ModelMapper().map(transfer, TransferenciaDto.class)).collect(Collectors.toList()));
+	}
+	
+	public Transferencia salvar(Transferencia transferencia) {
+		calculoTaxa(transferencia);
+		return transferenciaRepository.save(transferencia);
 	}
 	
 }

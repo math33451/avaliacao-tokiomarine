2f674135680c1879f86856bfc0944c45ace4e3f9
Matheus Bezerra <murilomatheus20@gmail.com>
2022-05-11 10:48:17 -0300

    limitando tamanho do número da conta

--- src/main/java/com/marinetokio/model/Transferencia.java
+++ src/main/java/com/marinetokio/model/Transferencia.java
@@ -1,15 +1,19 @@
 package com.marinetokio.model;
 
+import javax.persistence.Column;
 import javax.persistence.Id;
 
 import lombok.Data;
+
 
 @Data
 public class Transferencia {
 
 	@Id
 	private Long id;
+	@Column(length = 6)
 	private Long contaOrigem;
+	@Column(length = 6)
 	private Long contaDestino;
 	private Long valor;
 	private Long dataTransferencia;

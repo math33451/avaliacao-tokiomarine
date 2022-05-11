b7309f6ca480488649a03ea9c5918d193a95291c
Matheus Bezerra <murilomatheus20@gmail.com>
2022-05-11 11:54:21 -0300

    completando controller

--- src/main/java/com/marinetokio/controller/TransferenciaController.java
+++ src/main/java/com/marinetokio/controller/TransferenciaController.java
@@ -1,9 +1,19 @@
 package com.marinetokio.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marinetokio.dto.TransferenciaDto;
+import com.marinetokio.model.Transferencia;
 import com.marinetokio.repository.TransferenciaRepository;
+import com.marinetokio.service.TransferenciaService;
 
 import lombok.AllArgsConstructor;
 
@@ -13,6 +23,20 @@ import lombok.AllArgsConstructor;
 public class TransferenciaController {
 	
 	TransferenciaRepository transferenciaRepository;
+	TransferenciaService transferenciaService;
+	
+	@GetMapping("/listar")
+	public List<TransferenciaDto> listar(){
+		List<TransferenciaDto> transferencia = transferenciaService.listar();
+		return transferencia;
+	}
+	
+	@PostMapping("/salvar")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Transferencia salvar(@RequestBody Transferencia transferencia) {
+		return transferenciaService.salvar(transferencia);
+	}
+	
 	
 
 }

9bbcd3f47c89fd29ababfc4ca1683ad96aea8d50
Matheus Bezerra <murilomatheus20@gmail.com>
2022-05-12 01:46:19 -0300

    alterações página principal

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Sitema de Transferências"/>
 </template>
 
 <script>
@@ -20,7 +19,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: whitesmoke;
+  background-color: black;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,46 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <li>
+    <p>Conta Origem</p>
+    <input type="text" name="contaOrigem" size="30" /> 
+    </li>
+    <li>
+    <p>Conta Destino</p>
+    <input type="text" name="contaDestino" size="30" /> 
+    </li>
+    <li>
+    <p>Valor</p>
+    <input type="text" name="valor" size="30" /> 
+    </li>
+    <li>
+    <p>Dia da Transferência</p>
+    <input type="text" name="dataTransferência" size="30" /> 
+    </li>
+    <li>
+    <p>Dia de Hoje</p>
+    <input type="text" name="dataAgendada" size="30" /> 
+    </li>
+    <li>
+    <p>Tipo de Operação (1, 2, 3 ou 4)</p>
+    <input type="text" name="dataAgendada" size="30" /> 
+    </li>
+    <div>
+      <input type="submit" value="Transferir">
+    </div>
+    <div>
+      <input type="submit" value="Listar Transferências">
+    </div>
+    <h3>Instruções:</h3>
+    <div>Informe a conta de origem e a conta de destino.</div>
+    <div>Informe o valor a ser enviado.</div>
+    <div>Informe a data para a transferência ser realizada (dia de hoje + dias até a data da transferência, ex: 36)</div>
+    <div>Selecione o tipo de operação, sendo:</div>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    <div2><p>1- Operações tipo A:</p> são as agendadas para hoje.</div2>
+    <div2><p>2- Operações tipo B:</p> são as agendadas para até 10 dias.</div2>
+    <div2><p>3- Operações tipo C:</p> são as agendadas para após 10 dias.</div2>
+    <div3><p>4- Operações tipo D:</p> são as taxadas de acordo com seu valor.</div3>
     </ul>
   </div>
 </template>
@@ -45,14 +60,32 @@ h3 {
   margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+  padding: 10px;
+  border: 2px solid black;
+  display: flex;
 }
 li {
   display: inline-block;
   margin: 0 10px;
+  border-left: 1.5px solid black;
+  border-right: 1.5px solid black;
 }
 a {
   color: #42b983;
+}
+div{
+  margin: 10px;
+}
+
+div2{
+  margin: 10px;
+  border-left: 2px solid white;
+}
+
+div3{
+  margin: 10px;
+  padding: 10px;
+  border-right: 2px solid white;
+  border-left: 2px solid white;
 }
 </style>
